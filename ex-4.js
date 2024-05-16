c6d1a2a57239b15588a1a5e96ce478602905b368
Apirak Fakin <apirakfakin@gmail.com>
2024-05-16 13:27:24 +0700

    feat: add ex-4

--- ex-4.js
+++ ex-4.js
@@ -1,6 +1,8 @@
 // Exercise #4: Displaying Restaurant Name
-let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
+let restaurantNames = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
-// Start coding here
+for (let i of restaurantNames) {
+   newRestaurants.push(`Restaurant Name: ${i}`)
+}
 
 console.log(newRestaurants);
