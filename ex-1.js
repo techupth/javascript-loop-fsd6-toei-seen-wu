af0d46e550ac68399baf020c3e4f2908ef0e659a
Apirak Fakin <apirakfakin@gmail.com>
2024-05-16 13:10:31 +0700

    feat: add ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,8 @@
 // Exercise #1: Decrease the Student Scores
-let scores = [89, 70, 60, 50, 100, 71];
+let studentScores = [89, 70, 60, 50, 100, 71];
 
-// Start coding here
+for (let i of studentScores) {
+  i = i - 10;
+}
+
+console.log(studentScores);
